636bf8978776fa4ba1f110dfa380bacc3ba758f9
whosmeya <whosmeya@gmail.com>
2020-02-19 16:20:47 +0800

    feat: popup-bottom

---
+++ pages/popup-bottom-page/popup-bottom-page.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    visible: false,
+  },
+
+  handleShow: function () {
+    this.setData({ visible: true });
+  },
+
+  handleCancel: function () {
+    this.setData({ visible: false });
+  },
+})
